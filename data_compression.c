ef101274ad4e9407dc482eaffd7c9f789ee5db11
prasadchavann <prasadchavan1203@gmail.com>
2025-05-04 12:09:25 +0530

    Update data_compression.c

--- data_compression.c
+++ data_compression.c
@@ -1,3 +1,11 @@
+// a) Pick the first character from source string.
+// b) Append the picked character to the destination string.
+// c) Count the number of subsequent occurrences of the
+// picked character and append the count to destination string.
+// d) Pick the next character and repeat steps b) c) and d) if
+// end of string is NOT reach
+
+
 #include <stdio.h>
 #include <string.h>
 
